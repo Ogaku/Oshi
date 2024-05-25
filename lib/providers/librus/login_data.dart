5f951c2733238a8adb2278ba54357af855b807f1
公彦赤屋先 <kimihikoakaya@gmail.com>
2024-05-25 12:34:21 +0200

    Use platform-native http adapter for sync requests

--- lib/providers/librus/login_data.dart
+++ lib/providers/librus/login_data.dart
@@ -1,14 +1,16 @@
+import 'package:native_dio_adapter/native_dio_adapter.dart';
 import 'package:oshi/providers/librus/librus_login.dart';
 import 'package:oshi/providers/librus/librus_reader.dart';
 import 'package:cookie_jar/cookie_jar.dart';
 import 'package:dio/dio.dart';
 
 class SynergiaData {
-  SynergiaData() {
+  SynergiaData() : session = Dio() {
     librusApi = LibrusReader(this);
+    session.httpClientAdapter = NativeAdapter();
   }
 
-  Dio session = Dio();
+  Dio session;
   CookieJar cookieJar = CookieJar();
 
   LibrusLogin? synergiaLogin;
