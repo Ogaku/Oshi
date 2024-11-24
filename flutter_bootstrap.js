edb1f261000d72c95148b2e28c11847e647b4527
KimihikoAkayasaki <kimihikoakaya@gmail.com>
2024-11-24 13:08:59 +0000

    Deploying to gh-pages from  @ 29ecdd57f443caca6d7c0042b8eb5b32a6f934e5 🚀

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"235db911ba279722f5e685f38b0ed30fa7e857
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "73462023"
+    serviceWorkerVersion: "2495958770"
   }
 });
